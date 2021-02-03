0a6fb8e4965942eeabcde54d91038db5b3745ce9
Connor Murrell <cmurr311@gmail.com>
2021-02-03 00:12:48 -0600

    Car Classes

---
+++ Carprject/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Carprject
+{
+    public class Car
+    {
+        public Car()
+        {
+
+        }
+
+        public Car(string make, string model, int year)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+    }
+}
